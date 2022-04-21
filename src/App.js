38b518d2d916cabe63c53a5c295d77d3ead42f24
MaxwellChrist <ribthulu@gmail.com>
2022-04-21 15:57:40 -0400

    completed first stretch goal

--- src/App.js
+++ src/App.js
@@ -20,9 +20,15 @@ function App() {
 		setCart([...cart, item])
 	};
 
+	const removeItem = id => {
+		setCart(cart.filter(item => {
+			return item.id !== id
+		}))
+	}
+
 	return (
 		<div className="App">
-			<ProductContext.Provider value={{ products, addItem}}>
+			<ProductContext.Provider value={{ products, addItem, removeItem}}>
 				<CartContext.Provider value={{ cart }}>
 					<Navigation cart={cart} />
 					{/* Routes */}
